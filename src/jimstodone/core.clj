b37fd582220fe7795545c3102745551f3380fdea
Jim Suchy <jsuchy@gmail.com>
2011-07-09 13:37:17 -0500

    default page is now the list

--- src/jimstodone/core.clj
+++ src/jimstodone/core.clj
@@ -4,10 +4,11 @@
     [compojure.route :only (not-found)]
     [gaeshi.middleware.view-context :only (wrap-view-context)]
     [gaeshi.views :only (render-template render-html)]
+    [ring.util.response :only (redirect)]
     [gaeshi.controllers :only (controller-router)]))
 
 (defroutes jimstodone-routes
-  (GET "/" [] (render-template "index"))
+  (GET "/" [] (redirect "/list/index"))
   (controller-router 'jimstodone.controller)
   (not-found (render-template "not_found" :template-root "jimstodone/view" :ns `jimstodone.view.view-helpers)))
 
